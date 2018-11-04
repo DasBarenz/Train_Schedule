f6dc67599dc3e1e511400e356af4942be1271af2
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-11-03 21:57:14 -0600

    things work, for a change

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -17,8 +17,8 @@ $("#trainSubmit").on("click", function (event) {
     // Grabs user input
     var trainName = $("#trainInput").val().trim();
     var destination = $("#destinationInput").val().trim();
-    // var timeStart = moment($("#timeInput").val().trim(), "kk:mm"); BROKE M'SHTUFF
-    var timeStart = moment($("#timeInput").val().trim(), "kk:mm").format("X");
+    // var timeStart = moment($("#timeInput").val().trim(), "kk:mm");
+    var timeStart = moment($("#timeInput").val().trim(), "HH:mm").format("X");
     var frequency = $("#frequencyInput").val().trim();
 
     // Creates local "temporary" object for holding employee data
@@ -33,10 +33,10 @@ $("#trainSubmit").on("click", function (event) {
     database.ref().push(newTrain);
 
     // Logs everything to console
-    console.log("name " + newTrain.name);
-    console.log("where " + newTrain.where);
-    console.log("start " + newTrain.start);
-    console.log("freq " + newTrain.often);
+    console.log("name ", newTrain.name);
+    console.log("where ", newTrain.where);
+    console.log("start ", newTrain.start);
+    console.log("freq ", newTrain.often);
 
     // alert("Employee successfully added");
     //add an added notification on the screen
@@ -58,42 +58,59 @@ database.ref().on("child_added", function (childSnapshot) {
     var theStart = childSnapshot.val().start;
     var theFrequency = childSnapshot.val().often;
 
-    // Employee Info
-    console.log("DB name " + theName);
-    console.log("DB where " + theWhere);
-    console.log("DB start " + theStart);
-    console.log("DB freq " + theFrequency);
+    // train Info
+    console.log("DB name ", theName);
+    console.log("DB where ", theWhere);
+    console.log("DB start ", theStart);
+    console.log("DB freq ", theFrequency);
 
-    // Prettify the employee start
-    var startPretty = moment.unix(theStart).format("kk:mm");
+    //lisa
+//   var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+//   var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+//   var timeRemain = timeDiff % childSnapshot.val().frequency;
+//   var minToArrival = childSnapshot.val().frequency - timeRemain;
+//   var nextTrain = moment().add(minToArrival, "minutes");
+//   var key = childSnapshot.key;
 
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    var nextArrival = moment().diff(moment(theStart, "kk:mm"), "months");
-    console.log("nextArrival " + nextArrival);
 
-    // Calculate the total billed rate
-    var minutesAway = theStart & theFrequency;
-    console.log("minutesAway " + minutesAway);
+    //just for console log viewability
+    var startClean = moment.unix(theStart).format("HH:mm");
+    console.log("start clean", startClean)
+
+    //start time from DB and converted/manipulated
+    var startPretty = moment(theStart, "HH:mm").subtract(1, "years");
+    console.log("start pretty", startPretty)
+
+    // Current Time
+    var currentTime = moment().format("HH:mm");
+    console.log("CURRENT TIME: " + currentTime);
+
+    // Difference between the times
+    // var diffTime = moment(currentTime).diff(moment(startPretty), "minutes");
+    //var diffTime = moment().diff(moment(startPretty), "minutes");
+    var diffTime = moment().diff(moment(startPretty), "minutes");
+    console.log("DIFFERENCE IN TIME: ", moment(startPretty), "minutes");
+
+    //divide DIFFERENCE by frequency,REMAINDER is MINUTESAWAY
+    var minutesAway = diffTime % theFrequency;
+    console.log("remainder " + minutesAway);
+
+    //NEXTARRIVAL is current time plus MINUTESAWAY
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("HH:mm"));
+
+    //moment.max --> help with train start in future -- look at this component when i am first updating DB with start time
+
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(theName),
         $("<td>").text(theWhere),
-        $("<td>").text(startPretty),
-        $("<td>").text(nextArrival),
+        $("<td>").text(theFrequency),
+        $("<td>").text(moment(nextArrival).format("HH:mm")),
         $("<td>").text(minutesAway),
-        //   $("<td>").text(empBilled)
     );
 
-    // Append the new row to the table ... the > means create this (after) inside this (before)
-    $("#employee-table > tbody").append(newRow);
+    // // Append the new row to the table ... the > means create this (after) inside this (before)
+    $("#train-table > tbody").append(newRow);
 });
-
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
